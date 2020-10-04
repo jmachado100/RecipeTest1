7e470aa425a83dc92e6ff49c7181cc92ab01ff5d
jose <jose-machado96@hotmail.com>
2020-10-04 11:49:36 +0100

    Test

--- app/src/main/java/com/example/recipetest/MainActivity.kt
+++ app/src/main/java/com/example/recipetest/MainActivity.kt
@@ -2,10 +2,16 @@ package com.example.recipetest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btn1.setOnClickListener {
+            Toast.makeText(this, "Just a Test", Toast.LENGTH_SHORT).show()
+        }
     }
 }
